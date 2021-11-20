f3b61c9567758b191e9dbc62c98fc6acfa7c5887
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-20 15:30:01 -0800

    implemented solution code

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -5,10 +5,38 @@
 var Messages = {
 
   // TODO: Define how you want to store your messages.
-  _data: [],
+  _data: {},
 
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
   // _retrieve:
 
+  items: function() {
+    return _.chain(Object.values(Messages._data).sortBy('createdAt'));
+  },
+
+
+  add: function(message, callback) {
+    Messages._data[message.message_id] = message;
+    callback(Messages.items());
+  },
+
+  update: function(messages, callback) {
+    var length = Object.keys(Messages._data).length;
+
+    for (const message of messages) {
+      Messages._data[message.message_id] = Messages._conform(message);
+    }
+
+    if (Object.keys(Messages._data).length !== length) {
+      callback(Messages.items());
+    }
+  },
+
+  _conform: function(message) {
+    message.text = message.text || '';
+    message.username = message.username || '';
+    message.roomname = message.roomname || '';
+    return message;
+  }
 };
